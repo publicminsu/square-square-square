298e80c5435a3fc63e8e55925616f7cd2922e5d9
publicminsu <publicminsu@naver.com>
2023-12-19 04:03:03 +0900

    수정 : 큐브 AddForce제거, velocity로 대체

--- Assets/Scripts/Cube/CubeObject.cs
+++ Assets/Scripts/Cube/CubeObject.cs
@@ -14,12 +14,11 @@ public class CubeObject : MonoBehaviour
         //구 위의 랜덤 좌표
         Vector3 targetVector = Random.onUnitSphere;
 
-        //목표 좌표 바라보기
+        //목표 방향 바라보기
         Quaternion lookQuaternion = Quaternion.LookRotation(targetVector);
         transform.rotation = lookQuaternion;
 
-        //이전의 속도 제거 후 목표 좌표로 힘 가하기
-        cubeRigidbody.velocity = Vector3.zero;
-        cubeRigidbody.AddForce(targetVector, ForceMode.VelocityChange);
+        //목표 방향으로 속도 설정
+        cubeRigidbody.velocity = targetVector;
     }
 }
